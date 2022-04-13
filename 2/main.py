17096032e887e84f648259a13893189639bee7f0
Onur Can Yöndem <99740071+onur-yondem@users.noreply.github.com>
2022-04-13 12:43:52 +0300

    Update V1

--- 2/main.py
+++ 2/main.py
@@ -1,36 +1,38 @@
 class Error(Exception):
     pass
 
-class digitMismatch(Error):
+class DigitMismatch(Error):
     pass
 
-def sayiAtama(number):
+def sayi_atama(number):
     try:
         if isinstance(number, int):
             if number / 10 >= 1 and number / 10 < 10 or number / 10 < -1 and number / 10 > -10:
                 two_digit_number = number
-                sayiOkunusu(two_digit_number)
+                sayi_okunusu(two_digit_number)
+
             else:
-                raise digitMismatch
+                raise DigitMismatch
+
         else:
             raise TypeError
 
-    except digitMismatch:
+    except DigitMismatch:
         print("Sayı basamağı 2'den küçük veya 2'den büyük olamaz")
 
     except TypeError:
         print("Girilen sayı integer olmalı")
 
-    except Exception as e:
-        print("Birşeyler ters gitti..." + e.__class__)
+    except Exception as error:
+        print("Birşeyler ters gitti..." + error.__class__)
 
-def sayiOkunusu(number):
-    second_digits_dict = {0:"",1:"bir",2:"iki",3:"üç",4:"dört",5:"beş",6:"altı",7:"yedi",8:"sekiz",9:"dokuz"}
-    first_digits_dict = {1:"on",2:"yirmi",3:"otuz",4:"kırk",5:"elli",6:"altmış",7:"yetmiş",8:"seksen",9:"doksan"}
+def sayi_okunusu(number):
+    second_digits_dict = {0:"", 1:"bir", 2:"iki", 3:"üç", 4:"dört", 5:"beş", 6:"altı", 7:"yedi", 8:"sekiz", 9:"dokuz"}
+    first_digits_dict = {1:"on", 2:"yirmi", 3:"otuz", 4:"kırk", 5:"elli", 6:"altmış", 7:"yetmiş", 8:"seksen", 9:"doksan"}
 
     first_digit = int(abs(number)/10)
     second_digit = abs(number)%10
     stringNumber = first_digits_dict[first_digit] + second_digits_dict[second_digit]
     print("eksi " + stringNumber if number < 0 else stringNumber)
 
-sayiAtama(-99.5)
+sayi_atama(-99.5)
