f12c63fe4e10cf5d55688751d95d1f0df4482e1d
Onur Can Yöndem <99740071+onur-yondem@users.noreply.github.com>
2022-04-13 12:43:13 +0300

    Update V1

--- 1/main.py
+++ 1/main.py
@@ -4,19 +4,23 @@ class Error(Exception):
 class SortError(Error):
     pass
 
-def bolunenSayiBulma(min_sayi,max_sayi,bolunecek_sayi):
+def bolunen_sayi_bulma(min_sayi, max_sayi, bolunecek_sayi):
     try:
         number_list = []
-        if isinstance(min_sayi,int) and isinstance(max_sayi,int) and isinstance(bolunecek_sayi,int):
+        if isinstance(min_sayi, int) and isinstance(max_sayi, int) and isinstance(bolunecek_sayi, int):
             if min_sayi < max_sayi:
                 for number in range(min_sayi, max_sayi):
                     if number % bolunecek_sayi == 0:
                         number_list.append(number)
+
                 print(number_list)
                 toplam_sayi = len(number_list)
+
                 return toplam_sayi
+
             else:
                 raise SortError
+
         else:
             raise TypeError
 
@@ -26,9 +30,9 @@ def bolunenSayiBulma(min_sayi,max_sayi,bolunecek_sayi):
     except SortError:
         print("minimum sayı maksimum sayıdan büyük olamaz")
 
-    except Exception as e:
-        print("Birşeyler ters gitti..." + e.__class__)
+    except Exception as error:
+        print("Birşeyler ters gitti..." + error.__class__)
 
 
 
-bolunenSayiBulma(-25,50,5)
+bolunen_sayi_bulma(-25, 50, 5)
