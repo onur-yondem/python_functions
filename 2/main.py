4f6501275f3908da51f294f241b6b8cd0fd11440
Onur Can Yöndem <99740071+onur-yondem@users.noreply.github.com>
2022-04-18 01:38:09 +0300

    Update V2

--- 2/main.py
+++ 2/main.py
@@ -1,8 +1,10 @@
 class Error(Exception):
     pass
 
+
 class DigitMismatch(Error):
     pass
+
 
 def sayi_atama(number):
     try:
@@ -23,9 +25,6 @@ def sayi_atama(number):
     except TypeError:
         print("Girilen sayı integer olmalı")
 
-    except Exception as error:
-        print("Birşeyler ters gitti..." + error.__class__)
-
 def sayi_okunusu(number):
     second_digits_dict = {0:"", 1:"bir", 2:"iki", 3:"üç", 4:"dört", 5:"beş", 6:"altı", 7:"yedi", 8:"sekiz", 9:"dokuz"}
     first_digits_dict = {1:"on", 2:"yirmi", 3:"otuz", 4:"kırk", 5:"elli", 6:"altmış", 7:"yetmiş", 8:"seksen", 9:"doksan"}
@@ -34,5 +33,6 @@ def sayi_okunusu(number):
     second_digit = abs(number)%10
     stringNumber = first_digits_dict[first_digit] + second_digits_dict[second_digit]
     print("eksi " + stringNumber if number < 0 else stringNumber)
+    
 
 sayi_atama(-99.5)
